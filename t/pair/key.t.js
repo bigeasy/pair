2551bd24852530c0a8a5920fff2de8d52a9d7959
Alan Gutierrez <alan@prettyrobots.com>
2013-12-27 22:07:27 -0800

    Test option hash with no key.
    Test option selection against a hash that does not have the sought key.
    
    See #13.

--- t/pair/key.t.js
+++ t/pair/key.t.js
@@ -1,6 +1,6 @@
 require('proof')(2, function (equal) {
     var pair = require('../..')
-    var record = pair.key('a', 0, [ { keyEncoding: 'utf-8' } ])
+    var record = pair.key('a', 0, [ {}, { keyEncoding: 'utf-8' } ])
     equal(record.value.toString(), 'a', 'value')
     equal(record.version, 0, 'version')
 })
