fe44208fa708533dac57b3e5395291af21ffd9d3
Alan Gutierrez <alan@prettyrobots.com>
2014-10-24 21:52:19 -0500

    Upgrade Proof to 0.0.50.
    Closes #35.

--- t/pair/deserialize.t.js
+++ t/pair/deserialize.t.js
@@ -1,25 +1,25 @@
-require('proof')(12, function (equal, ok) {
+require('proof')(12, function (assert) {
     var pair = require('../..'), key, record
 
     key = pair.deserialize.key(new Buffer('0 a'))
-    equal(key.value.toString(), 'a', 'key value')
-    equal(key.value.length, 1, 'key length')
-    equal(key.version, 0, 'key version')
+    assert(key.value.toString(), 'a', 'key value')
+    assert(key.value.length, 1, 'key length')
+    assert(key.version, 0, 'key version')
 
     record = pair.deserialize.record(new Buffer('0 put 1 az'))
-    equal(record.key.toString(), 'a', 'record key')
-    equal(record.key.length, 1, 'record key length')
-    equal(record.value.toString(), 'z', 'record value')
-    equal(record.value.length, 1, 'record value length')
-    equal(record.operation, 'put', 'record operation')
-    equal(record.version, 0, 'key version')
+    assert(record.key.toString(), 'a', 'record key')
+    assert(record.key.length, 1, 'record key length')
+    assert(record.value.toString(), 'z', 'record value')
+    assert(record.value.length, 1, 'record value length')
+    assert(record.operation, 'put', 'record operation')
+    assert(record.version, 0, 'key version')
 
     record = pair.deserialize.record(new Buffer('0 del 1 a'))
-    ok(record.value == null, 'value is null')
+    assert(record.value == null, 'value is null')
 
     record = pair.deserializer(new Buffer('0 put 1 az'))
-    equal(record.value, 'z')
+    assert(record.value, 'z')
 
     key = pair.deserializer(new Buffer('0 a'), true)
-    equal(key.value, 'a')
+    assert(key.value, 'a')
 })
