12131a5b8065da8581413a57c100ec3c00871b83
Alan Gutierrez <alan@prettyrobots.com>
2015-02-16 23:05:07 -0600

    Completely test bytewise compare.
    See #42.

--- t/pair/comparator.t.js
+++ t/pair/comparator.t.js
@@ -2,6 +2,6 @@ require('proof')(2, prove)
 
 function prove (assert) {
     var pair = require('../..')
-    assert(pair.compare(new Buffer('a'), new Buffer('z')) < 0, 'less than')
+    assert(pair.compare(new Buffer('aaa'), new Buffer('aaz')) < 0, 'less than')
     assert(pair.compare(new Buffer('a'), new Buffer('aa')) < 0, 'shorter than')
 }
